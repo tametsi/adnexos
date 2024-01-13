8f80847e66956554d401027907517e89b004d14a
tametsi <93092155+tametsi@users.noreply.github.com>
2024-01-13 10:40:46 +0100

    chore: move backend into sub-dir and add global prettier

--- backend/cmd/adnexos/main.go
+++ backend/cmd/adnexos/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 func main() {
@@ -16,6 +17,11 @@ func main() {
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
 		return nil
+	})
+
+	// enable automigration
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		Automigrate: true,
 	})
 
 	if err := app.Start(); err != nil {
