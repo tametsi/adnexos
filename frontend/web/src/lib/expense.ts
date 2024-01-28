9ec534acbc9fb74f5d433bd167292fa860518dc3
tametsi <93092155+tametsi@users.noreply.github.com>
2024-01-28 15:24:24 +0100

    fix(ui): expense calculation if `source` is not in `members`

--- frontend/web/src/lib/expense.ts
+++ frontend/web/src/lib/expense.ts
@@ -9,19 +9,22 @@ import type { RecordModel } from 'pocketbase';
 export const calculateExpense = (expense: RecordModel, me: string) => {
 	const f = new Intl.NumberFormat(undefined, { style: 'currency', currency: 'EUR' });
 
+	const amountPaid = me === expense.source ? expense.amount : 0;
 	const toPay =
-		expense.source === me
-			? -(expense.amount - expense.amount / expense.members?.length)
-			: expense.amount / expense.members?.length;
+		!expense.members?.includes(me) && amountPaid === 0
+			? 0
+			: expense.source === me
+				? -(expense.amount - expense.amount / expense.members?.length)
+				: expense.amount / expense.members?.length;
 
 	return {
 		/** formatted in provided currency */
 		amountDisplay: f.format(expense.amount / 100),
 		/** the amount paid by me (might be negative), `0` means it's paid by someone else */
-		amountPaid: me === expense.source ? expense.amount : 0,
+		amountPaid,
 		/** raw value in cents (integer (hopefully)), negative values => I get money back */
 		toPay,
 		/** inverted `toPay`!** formatted in provided currency, positve values => I get money back */
-		toPayDisplay: f.format(-toPay / 100),
+		toPayDisplay: f.format(((toPay === 0 ? 1 : -1) * toPay) / 100),
 	};
 };
