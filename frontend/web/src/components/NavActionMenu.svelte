41e4fa09e5e6311b7225dfeb352738358c1e66bb
tametsi <93092155+tametsi@users.noreply.github.com>
2024-02-24 18:47:20 +0100

    refactor(ui): extract nav action menu to conditionally show it

---
+++ frontend/web/src/components/NavActionMenu.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { MoreVerticalIcon } from 'lucide-svelte';
+</script>
+
+<div class="dropdown dropdown-end">
+	<div
+		tabindex="0"
+		class="btn btn-sm btn-circle btn-ghost"
+		role="button"
+		aria-label="More actions"
+	>
+		<MoreVerticalIcon />
+	</div>
+	<!-- svelte-ignore a11y-no-noninteractive-tabindex see https://bugs.webkit.org/show_bug.cgi?id=22261 -->
+	<ul
+		tabindex="0"
+		class="dropdown-content menu menu-lg bg-base-300 rounded-box z-20 w-52 p-2 shadow-md"
+	>
+		<slot />
+	</ul>
+</div>
