4ae1e0f57219ae83907d7988d927ed11442b27b8
tametsi <93092155+tametsi@users.noreply.github.com>
2024-03-03 12:57:18 +0100

    feat(ui): adjust bottom nav style + animation

---
+++ frontend/web/src/lib/transitions.ts
@@ -0,0 +1,19 @@
+const scaleAnim = {
+	old: {
+		name: 'scaleOut',
+		duration: '.1s',
+		easing: 'ease-in-out',
+		fillMode: 'both',
+	},
+	new: {
+		name: 'scaleIn',
+		duration: '.1s',
+		easing: 'ease-in-out',
+		fillMode: 'both',
+	},
+};
+
+export const scale = {
+	forwards: scaleAnim,
+	backwards: scaleAnim,
+};
