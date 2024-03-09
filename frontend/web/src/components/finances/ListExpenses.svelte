7a49653a84fdb99f49dd2c014205a3cfcd986dd2
tametsi <93092155+tametsi@users.noreply.github.com>
2024-03-09 21:10:59 +0100

    feat(ui): add groups filter for ListExpenses on finances page

--- frontend/web/src/components/finances/ListExpenses.svelte
+++ frontend/web/src/components/finances/ListExpenses.svelte
@@ -2,18 +2,63 @@
 	import Error from '@/components/Error.svelte';
 	import Loading from '@/components/Loading.svelte';
 	import ExpenseListDetails from '@/components/expenses/ExpenseListDetails.svelte';
+	import { error } from '@/lib/alert';
 	import pb, { auth } from '@/lib/pb';
+	import type { RecordModel } from 'pocketbase';
 
-	let req = $pb.collection('expenses').getList(1, 100, {
-		filter: `isSettled = false && (source = "${$auth?.id}" || members.id ?= "${$auth?.id}")`,
+	let groups: RecordModel[] = [],
+		filterGroups: string[] = [],
+		filterGroupsQuery = '';
+
+	$: {
+		if (filterGroups.length === 0) filterGroupsQuery = '';
+		else filterGroupsQuery = ` && (${filterGroups.map(x => `group = "${x}"`).join(' || ')})`;
+	}
+
+	$: req = $pb.collection('expenses').getList(1, 100, {
+		filter: `isSettled = false && (source = "${$auth?.id}" || members.id ?= "${$auth?.id}")${filterGroupsQuery}`,
 		sort: '-created',
 		expand: 'members,source,group',
 	});
+
+	$pb.collection('groups')
+		.getFullList({ fields: 'id,name' })
+		.then(x => (groups = x))
+		.catch(error('Failed to fetch groups.'));
 </script>
+
+{#if groups.length > 0}
+	<div class="px-2">
+		<p class="font-bold">Filter Groups</p>
+		<div class="flex gap-2 overflow-x-auto py-4">
+			{#each groups as x (x.id)}
+				<input
+					type="checkbox"
+					value={x.id}
+					bind:group={filterGroups}
+					aria-label={x.name}
+					class="btn btn-outline btn-sm rounded-badge"
+				/>
+			{/each}
+		</div>
+	</div>
+{/if}
 
 {#await req}
 	<Loading />
 {:then expenses}
+	<p class="p-2 text-sm opacity-80">
+		{#if expenses?.items?.length > 0}
+			Showing all expenses
+			{filterGroups.length > 0
+				? `from ${filterGroups.length} group${filterGroups.length === 1 ? '' : 's'}`
+				: ''}
+			you are involved in.
+		{:else}
+			No expenses found.
+		{/if}
+	</p>
+
 	<div class="flex flex-col gap-2">
 		{#each expenses?.items as expense}
 			<ExpenseListDetails {expense} showGroup />
