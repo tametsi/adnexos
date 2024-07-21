17a037cf3dadaf2476dec6eb56c5eaf19feb15ce
tametsi <93092155+tametsi@users.noreply.github.com>
2024-07-21 13:26:00 +0200

    fix(ui): redirect to login if token is invalid on refresh

--- frontend/web/src/components/RefreshToken.svelte
+++ frontend/web/src/components/RefreshToken.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
 	import consts from '@/consts';
 	import pb, { auth } from '@/lib/pb';
-	import { isTokenExpired } from 'pocketbase';
+	import { ClientResponseError, isTokenExpired } from 'pocketbase';
 	import { onMount } from 'svelte';
 
 	onMount(() => {
 		if (!$auth?.id || !isTokenExpired($pb.authStore.token, consts.TOKEN_REFRESH_THRESHOLD))
 			return;
 
-		$pb.collection('users').authRefresh({ expand: 'settings_via_user' });
+		$pb.collection('users')
+			.authRefresh({ expand: 'settings_via_user' })
+			.catch((err: ClientResponseError) => {
+				if (err.status != 401) return;
+
+				// token is invalid
+				$pb.authStore.clear();
+
+				const origin = encodeURIComponent(
+					`${window.location.pathname}${window.location.search}`,
+				);
+				window.location.replace(`/login?redirect=${origin}`);
+			});
 	});
 </script>
