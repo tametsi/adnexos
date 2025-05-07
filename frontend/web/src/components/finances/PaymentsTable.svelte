0b5d423b91be7a01117326ec19e09c9f89331cb2
tametsi <93092155+tametsi@users.noreply.github.com>
2025-05-07 10:10:48 +0200

    fix(ui): invalid HTML structure for payments table

--- frontend/web/src/components/finances/PaymentsTable.svelte
+++ frontend/web/src/components/finances/PaymentsTable.svelte
@@ -39,32 +39,32 @@
 	{#each payments as payment}
 		{@const from = payment.to === $auth?.id}
 
-		<tr>
-			<th class="w-0">
-				{#if from}
-					<button
-						type="button"
-						onclick={remove(payment)}
-						class="btn btn-square btn-ghost btn-sm"
-					>
-						<span class="sr-only">Payment received</span>
-						<CheckIcon size="18" />
-					</button>
-				{/if}
-			</th>
-
-			<td class="truncate whitespace-nowrap">
-				<small>{from ? 'from' : 'to'}</small>
-				<span class="font-bold">
-					{from ? payment.expand?.from?.name : payment.expand?.to?.name}
-				</span>
-			</td>
-
-			<td class="whitespace-nowrap text-right">
-				<span class:text-error={!from} class:text-success={from}>
-					{f.format(((from ? 1 : -1) * payment.amount) / 100)}
-				</span>
-			</td>
-		</tr>
+		<tbody>
+			<tr>
+				<th class="w-0">
+					{#if from}
+						<button
+							type="button"
+							onclick={remove(payment)}
+							class="btn btn-square btn-ghost btn-sm"
+						>
+							<span class="sr-only">Payment received</span>
+							<CheckIcon size="18" />
+						</button>
+					{/if}
+				</th>
+				<td class="truncate whitespace-nowrap">
+					<small>{from ? 'from' : 'to'}</small>
+					<span class="font-bold">
+						{from ? payment.expand?.from?.name : payment.expand?.to?.name}
+					</span>
+				</td>
+				<td class="whitespace-nowrap text-right">
+					<span class:text-error={!from} class:text-success={from}>
+						{f.format(((from ? 1 : -1) * payment.amount) / 100)}
+					</span>
+				</td>
+			</tr>
+		</tbody>
 	{/each}
 </table>
