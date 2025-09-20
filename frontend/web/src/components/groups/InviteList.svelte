4ab696802e17c4f10d54eca9e2e02f88163ceb08
tametsi <93092155+tametsi@users.noreply.github.com>
2025-09-20 16:54:47 +0200

    feat(ui): update invite list
    - add a11y text
    - use daisyUI list element
    - add visual confirmation upon link copy

--- frontend/web/src/components/groups/InviteList.svelte
+++ frontend/web/src/components/groups/InviteList.svelte
@@ -3,7 +3,7 @@
 	import Loading from '@/components/Loading.svelte';
 	import { error } from '@/lib/alert';
 	import pb, { auth } from '@/lib/pb';
-	import { CopyIcon, Trash2Icon } from 'lucide-svelte';
+	import { CheckIcon, CopyIcon, MailIcon, Trash2Icon } from 'lucide-svelte';
 	import type { RecordModel } from 'pocketbase';
 	import { onMount } from 'svelte';
 
@@ -11,7 +11,8 @@
 		/** determine whether to show the delete btn */
 		groupOwner = $state(''),
 		req = $state(new Promise(() => {})),
-		invites: RecordModel[] = $state([]);
+		invites: RecordModel[] = $state([]),
+		copiedInviteId = $state<string | undefined>(undefined);
 
 	onMount(() => {
 		groupId = new URLSearchParams(window.location.search).get('id') || '';
@@ -34,7 +35,11 @@
 			.then(x => (invites = [...invites, x]))
 			.catch(error('Failed to create invite.'));
 	};
-	const copy = (id: string) => () => navigator.clipboard.writeText(buildInvite(id));
+	const copy = (id: string) => () =>
+		navigator.clipboard
+			.writeText(buildInvite(id))
+			.then(() => (copiedInviteId = id))
+			.catch(error('Failed to add to system clipboard.'));
 	const remove = (id: string) => () =>
 		$pb
 			.collection('invites')
@@ -51,20 +56,42 @@
 	<Error />
 {/await}
 
-{#each invites as invite (invite.id)}
-	<div class="flex items-center justify-between gap-2 py-1">
-		<span class="shrink truncate font-bold">{invite.id}</span>
+<ul class="list">
+	{#each invites as invite (invite.id)}
+		{@const copied = copiedInviteId === invite.id}
 
-		<div class="flex shrink-0 gap-2">
-			<button onclick={copy(invite.id)} class="btn btn-square btn-soft">
-				<CopyIcon />
+		<li class="list-row items-center gap-2 px-0">
+			<div class="p-2"><MailIcon /></div>
+
+			<div>
+				<p class="font-semibold">
+					<span class="sr-only">Invite-ID: </span>
+					{invite.id}
+				</p>
+				<div class="text-base-content/70 text-xs">
+					Expires: {new Date(invite.expires).toLocaleString()}
+				</div>
+			</div>
+
+			<button
+				onclick={copy(invite.id)}
+				class="btn btn-square btn-soft"
+				class:btn-success={copied}
+			>
+				<span class="sr-only">Copy Invite Link</span>
+				{#if copied}
+					<CheckIcon />
+				{:else}
+					<CopyIcon />
+				{/if}
 			</button>
 
 			{#if $auth?.id === invite.creator || $auth?.id === groupOwner}
 				<button onclick={remove(invite.id)} class="btn btn-square btn-soft btn-error">
+					<span class="sr-only">Delete Invite</span>
 					<Trash2Icon />
 				</button>
 			{/if}
-		</div>
-	</div>
-{/each}
+		</li>
+	{/each}
+</ul>
