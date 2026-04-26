10b809c99e00da8a9d10d9fa78ef61e96bdb5b91
tametsi <93092155+tametsi@users.noreply.github.com>
2026-04-26 16:04:56 +0200

    feat(ui): add excellent currency select on group create

--- frontend/web/src/components/groups/CreateGroupCard.svelte
+++ frontend/web/src/components/groups/CreateGroupCard.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import DialogCard from '@/components/DialogCard.svelte';
 	import { error } from '@/lib/alert';
+	import { CURRENCIES } from '@/lib/currency';
 	import pb, { auth } from '@/lib/pb';
 
 	let data = $state({
 		name: '',
+		currency: 'EUR',
 		owner: $auth?.id,
 	});
 	const create = () =>
@@ -32,9 +34,36 @@
 		/>
 	</label>
 
+	<!-- currency -->
+	<label class="fieldset">
+		<span class="label">Currency</span>
+
+		<select bind:value={data.currency} required placeholder="Currency" class="select w-full">
+			{#each CURRENCIES as currency}
+				{@const formatterText = new Intl.NumberFormat(undefined, {
+					style: 'currency',
+					currency,
+					currencyDisplay: 'name',
+				})}
+				{@const formatterSymbol = new Intl.NumberFormat(undefined, {
+					style: 'currency',
+					currency,
+					currencyDisplay: 'symbol',
+				})}
+
+				<option value={currency}>
+					{formatterText.formatToParts(0).find(x => x.type === 'currency')?.value}
+					({formatterSymbol.formatToParts(0).find(x => x.type === 'currency')?.value})
+				</option>
+			{/each}
+		</select>
+
+		<span class="label text-wrap">This value cannot be changed.</span>
+	</label>
+
 	<!-- actions -->
 	{#snippet actions()}
 		<button type="submit" class="btn btn-primary">Create</button>
-		<a href="/groups" class="btn btn-ghost">Cancel :(</a>
+		<a href="/groups" class="btn btn-ghost">Back</a>
 	{/snippet}
 </DialogCard>
